019754f746974819a4f6fde4c1bbf17fa86c0a4c
Ryan Oaks <roaks3@gmail.com>
2014-11-29 20:59:39 -0800

    Added rest endpoints that pull data from mongo db

--- map-challenge-server/orderService.js
+++ map-challenge-server/orderService.js
@@ -1,4 +1,8 @@
 
+var databaseUrl = "orderdb"; 
+var collections = ["hubs", "orders"];
+var orderdb = require("mongojs").connect(databaseUrl, collections);
+
 var express = require("express");
 var bodyParser = require("body-parser");
 
@@ -6,8 +10,18 @@ var app = express();
 
 app.use(bodyParser.json());
 
-app.get('/test', function (req, res) {
-  res.send('Test');
+app.get('/hubs', function (req, res) {
+  var cursor = orderdb.hubs.find();
+  cursor.toArray(function(err, docs) {
+    res.send(docs);
+  });
+});
+
+app.get('/orders', function (req, res) {
+  var cursor = orderdb.orders.find();
+  cursor.toArray(function(err, docs) {
+    res.send(docs);
+  });
 });
 
 app.listen(9005);
