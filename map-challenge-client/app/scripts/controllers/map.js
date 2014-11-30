da6b7f80222a4c8fe4334c154d4e99071361a901
Ryan Oaks <roaks3@gmail.com>
2014-11-29 21:37:51 -0800

    Added basic google maps widget per google example

--- map-challenge-client/app/scripts/controllers/map.js
+++ map-challenge-client/app/scripts/controllers/map.js
@@ -24,4 +24,10 @@ angular.module('mapChallengeClientApp')
       console.log(response || "Request failed");
     });
 
+    var mapOptions = {
+      center: {lat: -34.397, lng: 150.644},
+      zoom: 8
+    };
+    var map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
+
   });
