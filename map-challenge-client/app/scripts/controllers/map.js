78c8bf9b23488eb1fb930c9ea53b6a0eb7a5e0f4
Ryan Oaks <roaks3@gmail.com>
2014-11-30 14:58:59 -0800

    Added orders to map in heatmap layer

--- map-challenge-client/app/scripts/controllers/map.js
+++ map-challenge-client/app/scripts/controllers/map.js
@@ -12,7 +12,15 @@ angular.module('mapChallengeClientApp')
   .controller('MapCtrl', function ($scope, $http, uiGmapGoogleMapApi) {
 
     $scope.hubs = [];
-    $scope.map = { center: { latitude: 37.7577, longitude: -122.4376 }, zoom: 12 };
+    $scope.orders = [];
+    $scope.map = { center: { latitude: 37.7577, longitude: -122.4376 }, zoom: 12,
+      heatLayerCallback: function (layer) {
+        var heatmapData = $scope.orders.map(function(order) {
+          return order.latlng;
+        });
+        layer.setData(new google.maps.MVCArray(heatmapData));
+      }
+    };
 
     $http({
       method: 'GET',
@@ -21,6 +29,20 @@ angular.module('mapChallengeClientApp')
     success(function(response) {
       angular.forEach(response, function(hub) {
         $scope.hubs.push({id: hub.id, location: {longitude: hub.long, latitude: hub.lat}});
+      });
+    }).
+    error(function(response) {
+      console.log("error");
+      console.log(response || "Request failed");
+    });
+
+    $http({
+      method: 'GET',
+      url: 'http://localhost:9005/orders'
+    }).
+    success(function(response) {
+      angular.forEach(response, function(order) {
+        $scope.orders.push({id: order.id, latlng: new google.maps.LatLng(order.latitude, order.longitude)});
       });
     }).
     error(function(response) {
